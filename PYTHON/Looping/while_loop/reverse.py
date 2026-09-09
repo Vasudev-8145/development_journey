834dd3721d74bcb1eddcc896e5142a2f96fe903d
vasudev <vasudevc8145@gmail.com>
2026-09-09 22:02:05 +0530

    last digit

--- PYTHON/Looping/while_loop/reverse.py
+++ PYTHON/Looping/while_loop/reverse.py
@@ -5,7 +5,8 @@ Display reverse of a user input number
 num = int(input("Enter a number:"))
 rev = 0
 
-while num>0:
+while num!=0:
+
     last_digit = num%10
     rev = rev*10+last_digit
     num = num//10
